f74a4c3f5d2b798b063ffef7239b3297a4f94484
unknown <wilson121yer@gmail.com>
2024-07-23 10:08:17 -0500

    driver

--- app/booking.py
+++ app/booking.py
@@ -34,8 +34,8 @@ class BookingSearch:
         cls._url = "https://www.booking.com"
         options = webdriver.FirefoxOptions()
         options.add_argument("--headless")
-        #options.add_argument("disable-gpu")
-        #options.add_argument("no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
 
         return webdriver.Firefox(executable_path=os.path.abspath("geckodriver"), options=options)
     
