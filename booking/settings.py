89a2a974a3f628a5a849265cf4673dcd17f80d50
unknown <wilson121yer@gmail.com>
2024-08-28 14:07:19 -0500

    db

--- booking/settings.py
+++ booking/settings.py
@@ -85,12 +85,25 @@ WSGI_APPLICATION = 'booking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DB_PROD = True
+if not DB_PROD:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',  # Utiliza el backend de MySQL
+            'NAME': 'booking_bot',   # Nombre de la base de datos
+            'USER': 'root',                   # Usuario de la base de datos
+            'PASSWORD': 'KazYCK5o',            # Contraseña del usuario
+            'HOST': 'localhost',                    # Host, si es local usa 'localhost'
+            'PORT': '3306',                         # Puerto de MariaDB, usualmente 3306
+        }
+    }
 
 
 # Password validation
