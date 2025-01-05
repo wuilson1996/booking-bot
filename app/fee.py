4bcdeeeaaf48781668bafc2fe5cd9f414811d860
wuilson1996 <wilson121yer@gmail.com>
2025-01-05 16:09:40 -0500

    fee

--- app/fee.py
+++ app/fee.py
@@ -3,7 +3,7 @@ import random
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 from selenium.webdriver.common.keys import Keys
-from time import sleep
+from time import sleep, time
 import pandas as pd
 import logging
 import json
@@ -12,12 +12,6 @@ import datetime
 import re
 import os
 import platform
-
-_logging = logging.basicConfig(filename="logger.log", level=logging.INFO)
-
-pattern = r"\b(19\d\d|20\d\d)[-/](0[1-9]|1[0-2])[-/](0[1-9]|[12]\d|3[01])\b"
-def search_date(text):
-    return re.findall(pattern, text)
 
 class FeeTask:
     _url = "https://app.roompricegenie.com/client/calendar"
@@ -49,26 +43,35 @@ class FeeTask:
             return cls._driver_firefox()
 
     @classmethod
-    def controller(cls, driver:webdriver.Chrome, price, tipo, _date:str, username, password):
+    def controller(cls, driver, price, tipo, _date:str, username, password):
         try:
             driver.get(cls._url)
             driver.implicitly_wait(15)
             driver.delete_all_cookies()
 
             driver.find_element_by_xpath("//input[@type='email']").send_keys(username)
+            logging.info(f"[+] Add username success...")
             driver.find_element_by_xpath("//input[@type='password']").send_keys(password)
+            logging.info(f"[+] Add username password...")
             driver.find_element_by_xpath("//button[@type='submit']").click()
+            logging.info(f"[+] click button login success...")
             sleep(5)
+            if "Ajustes de la cuenta" in driver.page_source:
+                logging.info(f"[+] Inicio sesion correctamente...")
+            __time = time()
             while True:
-                if "Optimizando..." not in driver.page_source:
+                logging.info(f"[+] verificando inicio de sesion...")
+                if "Optimizando..." not in driver.page_source or (time() - __time >= 180):
                     break
+                sleep(1)
+
             while True:
                 status = False
                 for b in driver.find_elements_by_xpath("//button[@data-state='closed']"):
                     if b.get_attribute("data-testid") != "" and b.get_attribute("data-testid") != None:
-                        #print(f"[+] Fecha: {str(b.get_attribute('data-testid'))}...")
+                        logging.info(f"[+] Fecha: {str(b.get_attribute('data-testid'))}...")
                         if _date == str(b.get_attribute("data-testid")):
-                            print(f"[+] Fecha encontrada: {str(b.get_attribute('data-testid'))}...")
+                            logging.info(f"[+] Fecha encontrada: {str(b.get_attribute('data-testid'))}...")
                             status = True
                             b.click()
                             sleep(3)
@@ -102,18 +105,19 @@ class FeeTask:
 
                                     sleep(1)
                                     driver.find_element_by_xpath("//button[@data-userflow-id='price-drawer-save-prices-button']").click()
+                                    logging.info(f"[+] Tarifa actualizado correctamente....")
                                     break
                             break
                 if status:
-                    print("[+] Encontrada...")
+                    logging.info("[+] Encontrada...")
                     break
                 else:
-                    print("[+] Aumenta calendario...")
+                    logging.info("[+] Aumenta calendario...")
                     driver.find_element_by_xpath("//button[@data-testid='toNextMonthButton']").click()
                     sleep(3)
         
         except Exception as e:
-            print("Error: "+str(e))
+            logging.info("Error: "+str(e))
     
     @classmethod
     def close(cls, driver):
