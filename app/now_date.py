cf664b950df367e4efee150c546c0d11dcb56546
wuilson1996 <wilson121yer@gmail.com>
2025-04-30 19:30:07 -0500

    change

--- app/now_date.py
+++ app/now_date.py
@@ -1,11 +1,11 @@
 #import datetime
 #from django.utils.timezone import now as nw
 from datetime import timedelta, datetime
-from django.utils.timezone import localtime
+from django.utils import timezone
 
 def now():
-    return localtime((datetime.now() + timedelta(hours=0)).astimezone())
+    return timezone.localtime((datetime.now() + timedelta(hours=0)).astimezone())
 
 def parse_created_to_localtime(created_str):
     naive_datetime = datetime.strptime(created_str, "%Y-%m-%d %H:%M:%S")  # Formato esperado: "2025-05-01 12:34:56"
-    return localtime((naive_datetime + timedelta(hours=0)).astimezone())
+    return timezone.make_aware(naive_datetime)
