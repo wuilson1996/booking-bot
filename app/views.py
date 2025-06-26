c8001686ec995fda6b988581a9796cfb062437e2
wuilson1996 <wilson121yer@gmail.com>
2025-06-26 01:33:43 -0500

    change

--- app/views.py
+++ app/views.py
@@ -1428,7 +1428,7 @@ def index(request):
                     try:
                         bookings[str(_date_from.date())][2]["media_general"] = round((round((bookings[str(_date_from.date())][2]["media_name_hotel"] / bookings[str(_date_from.date())][2]["media_cant_name_hotel"])) + round((bookings[str(_date_from.date())][2]["media_total"] / bookings[str(_date_from.date())][2]["media_cant"]))) / 2)
                     except Exception as e:
-                        generate_log(f"[X] Error view data: {e001}", BotLog.BOOKING)
+                        generate_log(f"[X] Error view data: {e}", BotLog.BOOKING)
             # calculo de precio 5 y 10 posicion de triples y suites
             if '4' in bookings[str(_date_from.date())][3].keys() and '4' in bookings[str(_date_from.date())][5].keys():
                 bookings[str(_date_from.date())][3]['4']["5_10"] = round((float(bookings[str(_date_from.date())][3]['4'][4]["price"]) + float(bookings[str(_date_from.date())][3]['4'][9]["price"])) / 2)
