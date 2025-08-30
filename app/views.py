5e62bf6007c89eddd47a05c3e8bea0d4326c447f
wuilson1996 <wilson121yer@gmail.com>
2025-08-30 09:44:30 -0500

    change

--- app/views.py
+++ app/views.py
@@ -330,8 +330,8 @@ def active_process(bot_setting:BotSetting):
     
     threads = [
         threading.Thread(target=active_process_sf_v2, args=(0, 1, "HILO_0_A_1_MESES")),
-        threading.Thread(target=active_process_sf_v2, args=(31, 2, "HILO_1_A_2_MESES")),
-        threading.Thread(target=active_process_sf_v2, args=(62, 4, "HILO_2_A_4_MESES")),
+        threading.Thread(target=active_process_sf_v2, args=(31, 3, "HILO_1_A_2_MESES")),
+        #threading.Thread(target=active_process_sf_v2, args=(62, 4, "HILO_2_A_4_MESES")),
         threading.Thread(target=active_process_sf)
         #threading.Thread(target=active_process_sf_v2, args=(62, 4, "HILO_2_A_4_MESES")),
         #threading.Thread(target=active_process_sf_v2, args=(122, 6, "HILO_4_A_6_MESES")),
